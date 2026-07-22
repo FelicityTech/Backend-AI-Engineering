4a08fd90e59fb147c9464d597f59558d5046e2bc
Solomon Adegoke <souceking@gmail.com>
2026-07-22 17:34:39 +0100

    Stage 0: hello server

--- FastAPI in Python/CRUD_in_FastAPI/main.py
+++ FastAPI in Python/CRUD_in_FastAPI/main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
 
-app = FastAPI()
-
-@app.get("/")
-async def root():
-    return {"message": "Hello, World!"}
-
+app = FastAPI(title="Task API", 
+              version="1.0"
+              )
