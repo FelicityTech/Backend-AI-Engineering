8db1aa05b6a03355220120146db936953be08294
Solomon Adegoke <souceking@gmail.com>
2026-07-30 15:32:28 +0100

    Stage 0: create SQLite database

---
+++ FastAPI in Python/A2_Connecting_to_database/main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, Depends
+from datetime import datetime, timezone
+from typing import Any, Annotated, Generic, TypeVar
+from contextlib import asynccontextmanager
+from fastapi import Request, HTTPException
+import random
+from pydantic import BaseModel
+import sqlite3
+from sqlmodel import select, Field, Session, SQLModel, create_engine
+
+
+
+
+class Tasks(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str = Field(index=True, max_length=100)
+    done: bool = Field(default=False, index=True)
+
+
+sqlite_file_name = "tasks.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    with Session(engine) as session:
+        if not session.exec(select(Tasks)).first():
+            session.add_all([
+                Tasks(title="Wake Up", done=True),
+                Tasks(title="Brush Teeth", done=True),
+                Tasks(title="Take Bath", done= False)
+            ])
+            session.commit()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+async def read_tasks(session: SessionDep):
+    data = session.exec(select(Tasks)).all()
+    return {"Tasks": data}
